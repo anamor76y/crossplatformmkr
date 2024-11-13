15c148a645c824e5465fa4162a856de859cd9ed8
anamor76y <anna.moroz@knu.ua>
2024-11-13 11:39:18 +0200

    initial commit

---
+++ mkr/FindLeght.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1
+{
+
+    public class FindLength
+    {
+        private static int Factorial(int x)
+        {
+            if (x <= 1) return 1;
+            return x * Factorial(x - 1);
+        }
+
+        public static int CalculatePosition(int N, int K, List<int> arrangement)
+        {
+            List<int> availableNumbers = Enumerable.Range(1, N).ToList();
+            int position = 0;
+
+            for (int i = 0; i < K; i++)
+            {
+                int number = arrangement[i];
+                int index = availableNumbers.IndexOf(number);
+
+                position += index * Factorial(N - i - 1) / Factorial(N - K);
+
+                availableNumbers.RemoveAt(index);
+            }
+
+            return position + 1; 
+        }
+    }
+}
---
+++ mkr/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lab1
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            var input = File.ReadAllLines("input.txt");
+            var nk = input[0].Split().Select(int.Parse).ToArray();
+            int N = nk[0];
+            int K = nk[1];
+            var arrangement = input[1].Split().Select(int.Parse).ToList();
+
+            int result = FindLength.CalculatePosition(N, K, arrangement);
+            Console.WriteLine("Calculated position: " + result);
+            string outputPath = "output1.txt";  
+            File.WriteAllText(outputPath, "Calculated position: " + result);
+        }
+    }
+}
